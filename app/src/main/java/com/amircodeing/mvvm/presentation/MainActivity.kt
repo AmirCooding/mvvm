c8bf95adf2b80ba355e3daf4583a5949c8836827
Amir <amirrllotfi@gmail.com>
2024-03-14 11:48:44 +0100

    project packaging based on MVVM Structure

--- app/src/main/java/com/amircodeing/mvvm/presentation/MainActivity.kt
+++ app/src/main/java/com/amircodeing/mvvm/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.amircodeing.mvvm
+package com.amircodeing.mvvm.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.amircodeing.mvvm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
